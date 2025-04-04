f82a8dc79eaba42a7b2d2f6597aff609ad08a86a
Elmertoro10 <gutierrezcaja123@gmail.com>
2025-04-04 16:46:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function jugar() {
+    alert("¡Vamos a jugar!");
+  }
+  
+  function opciones() {
+    alert("Opciones del juego...");
+  }
+  
+  function creditos() {
+    alert("Créditos del equipo...");
+  }
+  
